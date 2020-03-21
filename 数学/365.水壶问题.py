da30a882361b7b22162359db721697b5c263a41f
liuzheyuan <yyxt11@gmail.com>
2020-03-21 15:14:39 +0800

    365.水壶问题,DFS暴力搜索

---
+++ 数学/365.水壶问题.py
@@ -0,0 +1,35 @@
+#方法一.DFS暴力搜索法,这个有些案例会超时
+#遍历所有的操作,同时为了记录,用self.state记录当前状态
+#1.X满上
+#2.Y满上
+#3.X倒入Y中,直到Y满或者倒空
+#4.Y倒入X中,直到X满或者倒空
+#5.X倒空
+#6.Y倒空
+
+
+class Solution:
+    def canMeasureWater(self, x: int, y: int, z: int) -> bool:
+        stack = [(0,0)]
+        self.state = set()
+
+
+        while(stack):
+            remainX,remainY = stack.pop()
+
+            if remainX == z or remainY == z or remainY+remainX == z:
+                return True
+
+            if (remainX,remainY) in self.state:
+                continue
+
+            self.state.add((remainX,remainY))
+                
+            stack.append((x,remainY))
+            stack.append((remainX,y))
+            stack.append((x-min(remainX,y-remainY),remainY + min(remainX,y-remainY)))
+            stack.append((remainX + min(remainY,x-remainX),y-min(remainY,x-remainX)))
+            stack.append((0,remainY))
+            stack.append((remainX,0))
+
+        return False
